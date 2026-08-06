cb14aa358f14154350ae12dd7a179cd7682b0a4f
Rudra Pratapure <rudrapratapure1020@gmail.com>
2026-08-07 00:10:49 +0530

    Added application launcher tool

--- tools/app_launcher.py
+++ tools/app_launcher.py
@@ -1,8 +1,17 @@
 import subprocess
 
+apps = {
+    "notepad": "notepad",
+    "calculator": "calc",
+    "paint": "mspaint"
+}
+
+
 def open_app(app_name):
-    if app_name.lower() == "notepad":
-        subprocess.Popen("notepad")
+    app_name = app_name.lower()
+
+    if app_name in apps:
+        subprocess.Popen(apps[app_name])
 
     else:
         print(f"Sorry, I don't know how to open '{app_name}' yet.")
